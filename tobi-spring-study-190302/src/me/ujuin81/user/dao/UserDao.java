55aa49de2156cc6e68f6ce411202031216efd938
ujuin81 <ujuin81@gmail.com>
2019-03-02 17:01:09 +0900

    //connection 관심사 분리

--- tobi-spring-study-190302/src/me/ujuin81/user/dao/UserDao.java
+++ tobi-spring-study-190302/src/me/ujuin81/user/dao/UserDao.java
@@ -31,8 +31,7 @@ public class UserDao {
 	}
 	
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?verifyServerCertificate=false&useSSL=false", "spring", "book");
+		Connection c = getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
@@ -45,9 +44,8 @@ public class UserDao {
 		c.close();
 	}
 	
-	public User get(String id) throws ClassNotFoundException, SQLException{
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?verifyServerCertificate=false&useSSL=false", "spring", "book");
+	public User get(String id) throws ClassNotFoundException, SQLException{		
+		Connection c = getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
@@ -64,6 +62,13 @@ public class UserDao {
 		c.close();
 		
 		return user;
+	}
+	
+	//관심사 분리 
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?verifyServerCertificate=false&useSSL=false", "spring", "book");
+		return c;
 	}
 	
 }
