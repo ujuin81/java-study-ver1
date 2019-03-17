444624325431728a93f256cf138d0a05d8288e46
ujuin81 <ujuin81@gmail.com>
2019-03-17 16:09:00 +0900

    ch.7.5.3 : 트랜잭션 적용

--- toby-spring-study-190302/src/me/ujuin81/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
+++ toby-spring-study-190302/src/me/ujuin81/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
@@ -6,6 +6,10 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import me.ujuin81.user.sqlservice.SqlNotFoundException;
 import me.ujuin81.user.sqlservice.SqlUpdateFailureException;
@@ -13,11 +17,13 @@ import me.ujuin81.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 	JdbcTemplate jdbc;
-	
+	TransactionTemplate transactionTemplate;
+
 	public void setDataSource(DataSource dataSource) {
 		jdbc = new JdbcTemplate(dataSource);
+		transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
 	}
-	
+
 	@Override
 	public void registerSql(String key, String sql) {
 		jdbc.update("insert into sqlmap(key_, sql_) values(?, ?)", key, sql);
@@ -28,7 +34,7 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 	public String findSql(String key) throws SqlNotFoundException {
 		try {
 			return jdbc.queryForObject("select sql_ from sqlmap where key_ = ?", String.class, key);
-		}catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new SqlNotFoundException(key + "에 해당하는 SQL을 찾을 수 없습니다.");
 		}
 	}
@@ -36,16 +42,22 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 	@Override
 	public void updateSql(String key, String sql) throws SqlUpdateFailureException {
 		int affected = jdbc.update("update sqlmap set sql_ = ? where key_ = ?", sql, key);
-		if(affected == 0) {
-			throw new SqlUpdateFailureException(key + "에 해당하는 SQL을 찾을 수 없습니다."); 
+		if (affected == 0) {
+			throw new SqlUpdateFailureException(key + "에 해당하는 SQL을 찾을 수 없습니다.");
 		}
 	}
 
 	@Override
-	public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-		for(Map.Entry<String, String> entity : sqlmap.entrySet()) {
-			updateSql(entity.getKey(), entity.getValue());
-		}
+	public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException {
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				for (Map.Entry<String, String> entity : sqlmap.entrySet()) {
+					updateSql(entity.getKey(), entity.getValue());
+				}
+			}
+		});
 	}
 
 }
--- toby-spring-study-190302/src/me/ujuin81/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ toby-spring-study-190302/src/me/ujuin81/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -1,11 +1,17 @@
 package me.ujuin81.user.sqlservice.updatable;
 
+import static org.junit.Assert.fail;
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.After;
+import org.junit.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
+import me.ujuin81.user.sqlservice.SqlUpdateFailureException;
 import me.ujuin81.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
@@ -13,20 +19,35 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
 
 	@Override
 	protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
-		db = new EmbeddedDatabaseBuilder()
-				.setType(HSQL)
-				.addScript("classpath:/me/ujuin81/user/sqlservice/updatable/sqlRegistrySchema.sql")
-				.build();
-		
+		db = new EmbeddedDatabaseBuilder().setType(HSQL)
+				.addScript("classpath:/me/ujuin81/user/sqlservice/updatable/sqlRegistrySchema.sql").build();
+
 		EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
 		embeddedDbSqlRegistry.setDataSource(db);
-		
+
 		return embeddedDbSqlRegistry;
 	}
-	
+
 	@After
 	public void tearDown() {
 		db.shutdown();
+	}
+
+	@Test
+	public void transactionUpdate() {
+		checkFindResult("SQL1", "SQL2", "SQL3");
+
+		Map<String, String> sqlmap = new HashMap<String, String>();
+		sqlmap.put("KEY1", "Modified1");
+		sqlmap.put("KEY9999!@#$", "Modified9999");
+
+		try {
+			sqlRegistry.updateSql(sqlmap);
+			fail();
+		} catch (SqlUpdateFailureException e) {
+		}
+
+		checkFindResult("SQL1", "SQL2", "SQL3");
 	}
 
 }
