83754ff39c71d909448a208c5566a96ffee459cf
ujuin81 <ujuin81@gmail.com>
2019-03-18 15:32:05 +0900

    ch.7.6.5 : @PropertySource

--- toby-spring-study-190302/src/me/ujuin81/AppContext.java
+++ toby-spring-study-190302/src/me/ujuin81/AppContext.java
@@ -2,11 +2,17 @@ package me.ujuin81;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PlaceholderConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -21,24 +27,48 @@ import me.ujuin81.user.service.UserService;
 import me.ujuin81.user.service.UserServiceTest.TestUserService;
 
 @Configuration
-@EnableTransactionManagement 
-@ComponentScan(basePackages="me.ujuin81.user")
-@Import({SqlServiceContext.class})
+@EnableTransactionManagement
+@ComponentScan(basePackages = "me.ujuin81.user")
+@Import({ SqlServiceContext.class })
+@PropertySource("/database.properties")
 public class AppContext {
+	//@Autowired Environment env;
+	//↓↓↓↓↓↓↓↓↓↓↓↓ 둘 중 하나 선택해서 사용 
+	@Value("${db.driverClass}") Class<? extends java.sql.Driver> driverClass;
+	@Value("${db.url}") String url;
+	@Value("${db.username}") String username;
+	@Value("${db.password}") String password;
 	
-	/** 
+	//@Value 주입 사용하려면 필요 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	/**
 	 * DB 연결과 트랜잭션
 	 */
 
 	@Bean
 	public DataSource dataSource() {
 		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+		//try {
+		//	dataSource.setDriverClass(
+		//			(Class<? extends java.sql.Driver>)Class.forName(env.getProperty("db.driverClass")));
+		//} catch (ClassNotFoundException e) {
+		//	throw new RuntimeException(e);
+		//}
+		//dataSource.setUrl(env.getProperty("db.url"));
+		//dataSource.setUsername(env.getProperty("db.username"));
+		//dataSource.setPassword(env.getProperty("db.password"));
+		//↓↓↓↓↓↓↓↓↓↓↓↓ 둘 중 하나 선택해서 사용 
+		dataSource.setDriverClass(this.driverClass);
+		dataSource.setUrl(this.url);
+		dataSource.setUsername(this.username);
+		dataSource.setPassword(this.password);
 		
-		dataSource.setDriverClass(Driver.class);
-		dataSource.setUrl("jdbc:mysql://localhost/testdb?verifyServerCertificate=false&useSSL=false&characterEncoding=UTF-8");
-		dataSource.setUsername("spring");
-		dataSource.setPassword("book");
-		
+
 		return dataSource;
 	}
 
@@ -48,7 +78,7 @@ public class AppContext {
 		tm.setDataSource(dataSource());
 		return tm;
 	}
-	
+
 	@Configuration
 	@Profile("production")
 	public static class ProductionAppContext {
@@ -59,10 +89,10 @@ public class AppContext {
 			return mailSender;
 		}
 	}
-	
+
 	@Configuration
 	@Profile("test")
-	public static class TestAppContext {	
+	public static class TestAppContext {
 		@Bean
 		public UserService testUserService() {
 			return new TestUserService();
