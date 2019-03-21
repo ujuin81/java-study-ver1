c47ffe75828cdfb2d1539d0784d6e3246034d833
ujuin81 <ujuin81@gmail.com>
2019-03-21 18:56:45 +0900

    ch.1.1.2~3 : GenericApplicationContext, 컨텍스트 계층 구조

--- toby-spring-study-190302/src/me/ujuin81/learningtest/spring/ioc/ApplicationContextTest.java
+++ toby-spring-study-190302/src/me/ujuin81/learningtest/spring/ioc/ApplicationContextTest.java
@@ -9,45 +9,103 @@ import org.junit.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 
 import me.ujuin81.learningtest.spring.ioc.bean.Hello;
+import me.ujuin81.learningtest.spring.ioc.bean.Printer;
 import me.ujuin81.learningtest.spring.ioc.bean.StringPrinter;
 
 public class ApplicationContextTest {
+	private String basePath = StringUtils.cleanPath(ClassUtils.classPackageAsResourcePath(getClass())) + "/";
 
+	//ch.1.1.1 
 	@Test
 	public void registerBean() {
 		StaticApplicationContext ac = new StaticApplicationContext();
 		ac.registerSingleton("hello1", Hello.class);
-		
+
 		Hello hello1 = ac.getBean("hello1", Hello.class);
 		assertThat(hello1, is(notNullValue()));
-		
+
 		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
 		helloDef.getPropertyValues().addPropertyValue("name", "Spring");
 		ac.registerBeanDefinition("hello2", helloDef);
-		
+
 		Hello hello2 = ac.getBean("hello2", Hello.class);
 		assertThat(hello2.sayHello(), is("Hello Spring"));
 		assertThat(hello1, is(not(hello2)));
-		
+
 		assertThat(ac.getBeanFactory().getBeanDefinitionCount(), is(2));
 	}
-	
+
+	//ch.1.1.1 
 	@Test
 	public void registerBeanWithDependency() {
 		StaticApplicationContext ac = new StaticApplicationContext();
 		ac.registerBeanDefinition("printer", new RootBeanDefinition(StringPrinter.class));
-		
+
 		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
 		helloDef.getPropertyValues().addPropertyValue("name", "Spring");
 		helloDef.getPropertyValues().addPropertyValue("printer", new RuntimeBeanReference("printer"));
 		ac.registerBeanDefinition("hello", helloDef);
+
+		Hello hello = ac.getBean("hello", Hello.class);
+		hello.print();
+
+		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+	}
+
+	//ch.1.1.2
+	@Test
+	public void genericApplicationContext() {
+		GenericApplicationContext ac = new GenericApplicationContext();
+
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(ac);
+		reader.loadBeanDefinitions("me/ujuin81/learningtest/spring/ioc/genericApplicationContext.xml");
+
+		ac.refresh();
+
+		Hello hello = ac.getBean("hello", Hello.class);
+		hello.print();
+
+		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+	}
+
+	//ch.1.1.2
+	@Test
+	public void genericXmlApplicationContext() {
+		GenericXmlApplicationContext ac = new GenericXmlApplicationContext(
+				"me/ujuin81/learningtest/spring/ioc/genericApplicationContext.xml");
 		
 		Hello hello = ac.getBean("hello", Hello.class);
 		hello.print();
-		
+
 		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+	}
+	
+	//ch.1.1.3 : 컨텍스트 계층 구조 
+	@Test
+	public void contextHierachy() {
+		ApplicationContext parent = new GenericXmlApplicationContext(basePath + "parentContext.xml");
+		
+		GenericApplicationContext child = new GenericApplicationContext(parent); //<--- 연결
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+		reader.loadBeanDefinitions(basePath + "childContext.xml");
+		child.refresh();
+		
+		Printer printer = child.getBean("printer", Printer.class); //parent context에서 검색 
+		assertThat(printer, is(notNullValue()));
+		
+		Hello hello = child.getBean("hello", Hello.class); //child context에서 검색 
+		assertThat(hello, is(notNullValue()));
+		
+		hello.print();
+		assertThat(printer.toString(), is("Hello Child"));
 	}
 }
